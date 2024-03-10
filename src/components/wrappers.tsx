ae38bfbbfd6a145b8986db9b0e6d3ff837585157
Константин Александров <k.alexandrov@shtormtech.ru>
2024-03-10 15:20:06 +0300

    RTK

--- src/components/wrappers.tsx
+++ src/components/wrappers.tsx
@@ -1,3 +1,9 @@
 import React from "react";
+import { Provider } from "react-redux";
+import { store } from "../store/store";
 
-export const Wrappers = () => <React.StrictMode>123c</React.StrictMode>;
+export const Wrappers = () => (
+  <React.StrictMode>
+    <Provider store={store}>123c</Provider>
+  </React.StrictMode>
+);
